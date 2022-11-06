1cb40be921fea239e22ba12364ca72a2d924106d
abhigyan272 <103428624+abhigyan272@users.noreply.github.com>
2022-11-06 11:16:55 +0530

    Add files via upload

---
+++ SplashScreenActivity.java
@@ -0,0 +1,45 @@
+package com.example.budget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int SPLASH = 3300;
+    Animation topAnim, bottomAnim;
+    ImageView imageView;
+    TextView appName;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        imageView=findViewById(R.id.imageView);
+        appName=findViewById(R.id.text);
+
+        imageView.setAnimation(topAnim);
+        appName.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH);
+    }
+}
